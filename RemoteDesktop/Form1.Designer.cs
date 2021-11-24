928dfb4ce86cd2a4339de5c69ee916ca1c60799e
Denis Nechaev <denis180799@gmail.com>
2021-11-24 16:17:25 +0300

    [feature] Add screen switch

--- RemoteDesktop/Form1.Designer.cs
+++ RemoteDesktop/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.exitBtn = new System.Windows.Forms.Button();
             this.hideBtn = new System.Windows.Forms.Button();
+            this.switchBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // exitBtn
@@ -41,8 +42,9 @@
             this.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.exitBtn.ForeColor = System.Drawing.Color.White;
             this.exitBtn.Location = new System.Drawing.Point(0, 0);
+            this.exitBtn.Margin = new System.Windows.Forms.Padding(0);
             this.exitBtn.Name = "exitBtn";
-            this.exitBtn.Size = new System.Drawing.Size(160, 200);
+            this.exitBtn.Size = new System.Drawing.Size(160, 600);
             this.exitBtn.TabIndex = 0;
             this.exitBtn.Text = "Exit";
             this.exitBtn.UseVisualStyleBackColor = false;
@@ -58,27 +60,46 @@
             this.hideBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.hideBtn.ForeColor = System.Drawing.Color.White;
             this.hideBtn.Location = new System.Drawing.Point(160, 0);
+            this.hideBtn.Margin = new System.Windows.Forms.Padding(0);
             this.hideBtn.Name = "hideBtn";
-            this.hideBtn.Size = new System.Drawing.Size(160, 200);
+            this.hideBtn.Size = new System.Drawing.Size(480, 600);
             this.hideBtn.TabIndex = 1;
             this.hideBtn.Text = "Show Desktop";
             this.hideBtn.UseVisualStyleBackColor = false;
             this.hideBtn.Click += new System.EventHandler(this.hideBtn_Click);
+            // 
+            // switchBtn
+            // 
+            this.switchBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.switchBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.switchBtn.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.switchBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.switchBtn.ForeColor = System.Drawing.Color.White;
+            this.switchBtn.Location = new System.Drawing.Point(640, 0);
+            this.switchBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.switchBtn.Name = "switchBtn";
+            this.switchBtn.Size = new System.Drawing.Size(160, 600);
+            this.switchBtn.TabIndex = 2;
+            this.switchBtn.Text = "Switch screen";
+            this.switchBtn.UseVisualStyleBackColor = false;
+            this.switchBtn.Click += new System.EventHandler(this.button1_Click);
             // 
             // mainFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(320, 200);
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.switchBtn);
             this.Controls.Add(this.hideBtn);
             this.Controls.Add(this.exitBtn);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Location = new System.Drawing.Point(20, 20);
-            this.MinimumSize = new System.Drawing.Size(320, 200);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "mainFrm";
             this.Opacity = 0.8D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Remote Desktop";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
@@ -89,6 +110,7 @@
 
         private System.Windows.Forms.Button exitBtn;
         private System.Windows.Forms.Button hideBtn;
+        private System.Windows.Forms.Button switchBtn;
     }
 }
 
